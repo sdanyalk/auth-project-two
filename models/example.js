90302616846c86970fb60295f70f29bdeca4f826
SDK <s.danyal.k@gmail.com>
2019-11-23 23:29:46 -0700

    initial commit

---
+++ models/example.js
@@ -0,0 +1,8 @@
+module.exports = function (sequelize, DataTypes) {
+  const Example = sequelize.define("Example", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+
+  return Example;
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const db = require("./models");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+const syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
+    console.log(`==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
+  });
+});
+
+module.exports = app;
