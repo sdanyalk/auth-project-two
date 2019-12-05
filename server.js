1804763069e09125c5a9b70059e1ced2fe5b2d57
SDK <s.danyal.k@gmail.com>
2019-12-04 23:35:07 -0700

    force sync

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(authRoutes);
 app.use(userRoutes);
 app.use(historyRoutes);
 
-const syncOptions = { force: true };
+const syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
